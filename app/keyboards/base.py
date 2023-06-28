eda14089d6013cf8593c263814a50ea38b47b51e
valentinkelbakh <valentinkelbax@gmail.com>
2023-06-28 12:05:32 +0600

    Update base.py

--- app/keyboards/base.py
+++ app/keyboards/base.py
@@ -10,4 +10,9 @@ from app.data.states import Menu
 def get_back_btn(back_from):
     if back_from == Menu.Holidays:
         return InlineKeyboardButton(text='Назад', callback_data=cb.base_cb.new(option=Menu.Main, page=1))
-    return 1
+
+main_menu = InlineKeyboardMarkup()
+main_menu.add(InlineKeyboardButton(text='Немецкие праздники', 
+                                    callback_data=cb.base_cb.new(option=Menu.Holidays, page=1)))
+main_menu.add(InlineKeyboardButton(text='Ближайший праздник', 
+                                    callback_data=cb.base_cb.new(option=Menu.Recipes, page=1)))
