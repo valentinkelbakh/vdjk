791e3326a480ae3a8d68f4995ac16c40bf1cf4cb
valentinkelbakh <valentinkelbax@gmail.com>
2023-08-16 22:26:18 +0600

    returned previous prefix and separator
    for backward compatibility

--- app/data/callbacks.py
+++ app/data/callbacks.py
@@ -1,12 +1,12 @@
 from aiogram.filters.callback_data import CallbackData
 
 
-class BaseCallback(CallbackData, prefix="base"):
+class BaseCallback(CallbackData, prefix="post", sep='|'):
     option: str
     page: int
 
 
-class ExtendedCallback(CallbackData, prefix="ext"):
+class ExtendedCallback(CallbackData, prefix="post", sep='|'):
     option: str
     page: int
     data: str
