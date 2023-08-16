c52bbdd23077638dce9abdb32805657c8e12ca74
valentinkelbakh <valentinkelbax@gmail.com>
2023-08-16 16:49:13 +0600

    remove redunant format after start_text change

--- app/handlers/menu.py
+++ app/handlers/menu.py
@@ -11,11 +11,11 @@ from app.loader import dp
 @dp.message_handler(commands=['start', 'help', 'menu'], state='*')
 async def handle_start_help(message: types.Message, state: FSMContext = None):
     await state.finish()
-    text = kb.menu.start_text.format(message.chat.first_name)
+    text = kb.menu.start_text
     return await message.answer(text=text, reply_markup=kb.menu.main_menu)
 
 
 @dp.callback_query_handler(cb.base_cb.filter(option=Menu.MAIN), state='*')
 async def handle_back_to_menu(callback_query: types.CallbackQuery, state: FSMContext):
-    text = kb.menu.start_text.format(callback_query.message.chat.first_name)
+    text = kb.menu.start_text
     return await callback_query.message.edit_text(text=text, reply_markup=kb.menu.main_menu)
