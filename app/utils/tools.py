492781bdd38bab0e2b42bf762a10019ba82f9d19
valentinkelbakh <valentinkelbax@gmail.com>
2023-08-19 14:53:28 +0600

    add trimming functions to utils/tools

--- app/utils/tools.py
+++ app/utils/tools.py
@@ -1,6 +1,8 @@
 import logging
 
 # for various utility functions
+
+
 def json_get_by_id(json: list, id: int):
     """
     Find and return the first object in a JSON list that matches a given ID.
@@ -12,5 +14,36 @@ def json_get_by_id(json: list, id: int):
     Returns:
         dict or None: The first object in the JSON list that matches the given ID, or None if no match is found.
     """
-    
+
     return next((obj for obj in json if obj['id'] == id), None)
+
+
+def trim_str(string: str, length: int) -> str:
+    """
+    Trims a string to a given length if needed.
+
+    Parameters:
+        string (str): The string to trim.
+        length (int): The length to trim to.
+
+    Returns:
+        str: The trimmed string.
+    """
+    if len(string) > length:
+        return string[:length - 3] + '...'
+    return string
+
+
+def trim_for_button(text):
+    """ Trims text to fit telegram buttons """
+    return trim_str(text, 32)
+
+
+def trim_for_caption(text):
+    """ Trims text to fit telegram captions """
+    return trim_str(text, 1024)
+
+
+def trim_for_message(text):
+    """ Trims text to fit telegram message """
+    return trim_str(text, 4096)
