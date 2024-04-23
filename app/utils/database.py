812d1ffd2bf0a3f63016dec9161eef0155dd5598
valentinkelbakh <valentinkelbax@gmail.com>
2024-04-23 23:33:00 +0200

    split Data class into two variants depending in DB_WEBHOOK flag

--- app/utils/database.py
+++ app/utils/database.py
@@ -140,78 +140,98 @@ class Database:
         return response.json()
 
 
-class Data:
-    def __init__(self, db: Database) -> None:
-        self.__file_path = os.path.join(os.getcwd(), "app", "data", "data.json")
-        self.db = db
-        if DB_WEBHOOK:
-            self._holidays: list = []
-            self._recipes: list = []
-            self._projects: list = []
-            self.get_holidays = lambda: self._holidays
-            self.get_recipes = lambda: self._recipes
-            self.get_projects = lambda: self._projects
-            self.get_holiday = lambda id: json_get_by_id(self._holidays, id)
-            self.get_recipe = lambda id: json_get_by_id(self._recipes, id)
-            self.get_project = lambda id: json_get_by_id(self._projects, id)
-        else:
-            self.get_holidays = lambda: db.get(self.HOLIDAYS)
-            self.get_recipes = lambda: db.get(self.RECIPES)
-            self.get_projects = lambda: db.get(self.PROJECTS)
-            self.get_holiday = lambda id: db.get(self.HOLIDAYS, id)
-            self.get_recipe = lambda id: db.get(self.RECIPES, id)
-            self.get_project = lambda id: db.get(self.PROJECTS, id)
+if not DB_WEBHOOK:
 
-    def __load_data(self) -> None:
-        try:
-            with open(self.__file_path, "r", encoding="utf-8") as file:
-                data: dict = json.load(file)
-                self._holidays = data.get("holidays", {})
-                self._recipes = data.get("recipes", {})
-                self._projects = data.get("projects", {})
-                return
-        except FileNotFoundError:
-            logging.error("⭕Local data is not available, considering empty...⭕")
-        except Exception as e:
-            logging.error(f"⭕Error while loading data: {e}⭕")
-        # code below executes only if (any) exception occured
-        self._holidays = {}
-        self._recipes = {}
-        self._projects = {}
+    class Data:
+        def __init__(self, db: Database) -> None:
+            self.get_holidays = lambda: db.get(HOLIDAYS)
+            self.get_recipes = lambda: db.get(RECIPES)
+            self.get_projects = lambda: db.get(PROJECTS)
+            self.get_holiday = lambda id: db.get(HOLIDAYS, int(id))
+            self.get_recipe = lambda id: db.get(RECIPES, int(id))
+            self.get_project = lambda id: db.get(PROJECTS, int(id))
 
-    def __save_data(self) -> None:
-        data = {
-            "holidays": self._holidays,
-            "recipes": self._recipes,
-            "projects": self._projects,
-        }
-        with open(self.__file_path, "w", encoding="utf-8") as file:
-            json.dump(data, file)
+else:
 
-    async def update_async(self, update_subject="", seconds: int = 2):
-        """retrieves data from database and saves it to file
-        if database is not available and all data is empty, tries to load from file"""
-        logging.info("🔵 Retrieving data from database..")
+    class Data:
+        __file_path = os.path.join(os.getcwd(), "app", "data", "data.json")
+        _holidays: dict = {}
+        _recipes: dict = {}
+        _projects: dict = {}
 
-        # This ensures getting new data considering how Database works
-        await asyncio.sleep(seconds)
+        def __init__(self, db: Database) -> None:
+            self.db = db
 
-        if update_subject:
-            if update_subject in self.SUBJECTS:
-                try:
-                    data = self.db.get(update_subject)
-                except Exception as e:
-                    logging.error("⭕Database is not available" + "\n⭕ Error: {e} ⭕")
-                setattr(self, "_" + update_subject, data)
-            else:
-                logging.error(f'⭕Unknown subject "{update_subject}"⭕')
-        else:
+            self.get_holidays = lambda: self._holidays.values()
+            self.get_recipes = lambda: self._recipes.values()
+            self.get_projects = lambda: self._projects.values()
+            self.get_holiday = lambda id: self._holidays[int(id)]
+            self.get_recipe = lambda id: self._recipes[int(id)]
+            self.get_project = lambda id: self._projects[int(id)]
+
+        def load_from_file(self) -> None:
+            logging.info("🔵 Retrieving data from file..")
             try:
-                self._holidays = self.db.get(self.HOLIDAYS)
-                self._recipes = self.db.get(self.RECIPES)
-                self._projects = self.db.get(self.PROJECTS)
+                with open(self.__file_path, "r", encoding="utf-8") as file:
+                    data: dict = json.load(file)
+                    self._holidays = data.get("holidays", {})
+                    self._recipes = data.get("recipes", {})
+                    self._projects = data.get("projects", {})
+                    logging.info("🔵 Data was retrieved from file")
+                    return
+            except FileNotFoundError:
+                logging.error("⭕Local data is not available, considering empty...⭕")
             except Exception as e:
-                logging.error("⭕Database is not available" + "\n⭕ Error: {e} ⭕")
-                if not any((self._holidays, self._recipes, self._projects)):
-                    self.__load_data()
-        self.__save_data()
+                logging.error(f"⭕Error while loading data: {e}⭕")
+            # code below executes only if (any) exception occured
+            self._holidays = {}
+            self._recipes = {}
+            self._projects = {}
+
+        def __save_to_file(self) -> None:
+            data = {
+                "holidays": self._holidays,
+                "recipes": self._recipes,
+                "projects": self._projects,
+            }
+            with open(self.__file_path, "w", encoding="utf-8") as file:
+                json.dump(data, file)
+
+        async def update_async(self, update_subject="", seconds: int = 2):
+            """retrieves data from database and saves it to file
+            if database is not available and all data is empty, tries to load from file
+            """
+            logging.info("🔵 Retrieving data from database..")
+
+            # This ensures getting new data considering how Database works
+            await asyncio.sleep(seconds)
+
+            if update_subject:
+                if update_subject in SUBJECTS:
+                    try:
+                        data = Data.convert_to_dict(self.db.get(update_subject))
+                    except Exception as e:
+                        logging.error(
+                            "⭕Database is not available" + f"\n⭕ Error: {e} ⭕"
+                        )
+                        return
+                    setattr(self, "_" + update_subject, data)
+                else:
+                    logging.error(f'⭕Unknown subject "{update_subject}"⭕')
+            else:
+                try:
+                    self._holidays = Data.convert_to_dict(self.db.get(HOLIDAYS))
+                    self._recipes = Data.convert_to_dict(self.db.get(RECIPES))
+                    self._projects = Data.convert_to_dict(self.db.get(PROJECTS))
+                except Exception as e:
+                    logging.error("⭕Database is not available" + f"\n⭕ Error: {e} ⭕")
+                    if not any((self._holidays, self._recipes, self._projects)):
+                        self.load_from_file()
+                        return
+            self.__save_to_file()
+            logging.info("🔵 Data was retrieved from database")
+
+        @staticmethod
+        def convert_to_dict(_list: list) -> dict:
+            """converts list of elements to dict like {id: element}"""
+            return {d["id"]: d for d in _list}
